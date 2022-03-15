ccb85508a52eb63956823da8e5e32ca60efb8d04
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-15 19:42:11 +0100

    commit tree 77a8d4c4f81c2872d5bc33bb9291f2cc6b67d778

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun(); 
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(cat = "*") {
+    return function frogHome(frog = "special") {
+        return `You are ${cat}${frog}${cat}!`;
+    }
+}
+
+wrapAdjective();
